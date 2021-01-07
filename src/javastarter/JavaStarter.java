36d72ece1797f69a8dc422976aea8791ffaa18e9
azukhrufy <azukhrufy@outlook.com>
2021-01-07 15:22:48 +0700

    refine code for implementing sieve of eratosthenes

--- src/javastarter/JavaStarter.java
+++ src/javastarter/JavaStarter.java
@@ -15,27 +15,21 @@ public class JavaStarter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    int n = 20;
-
-    boolean[] isPrime = new boolean[n];
-   for (int i = 2; i < n; i++) {
-      isPrime[i] = true;
-   }
-   // Loop's ending condition is i * i < n instead of i < sqrt(n)
-   // to avoid repeatedly calling an expensive function sqrt().
-   for (int i = 2; i * i < n; i++) {
-//       System.out.println("i = " +i);
-      if (!isPrime[i]) continue;
-      for (int j = i * i; j < n; j += i) {
-//           System.out.println("j = " +j);
-         isPrime[j] = false;
-      }
-   }
-   int count = 0;
-   for (int i = 2; i < n; i++) {
-      if (isPrime[i]) count++;
-   }
-        System.out.println(count);
-        
+        int n = 20;
+        boolean[] isPrime = new boolean[n];
+        for(int i = 2; i < n; i++){
+            isPrime[i] = true;
+        }
+        for(int i = 2; i * i < n; i++){
+            if (!isPrime[i]) continue;
+            for(int j = i * i; j <n; j = j+i){
+                isPrime[j] = false;
+            }
+        }
+        int counter = 0;
+        for(int i = 2; i < n; i++){
+            if(isPrime[i]) counter++;
+        }
+        System.out.println("Prime Number Count = " + counter);
     }
-}
+  }
